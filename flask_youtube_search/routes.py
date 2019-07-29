e0511245e70ea4c7bee271e9375d2acddb7ea297
JINKA RANGA BHARATH <bharathjinka09@gmail.com>
2019-07-29 22:59:04 +0530

    Added Flask youtube API Results

---
+++ flask_youtube_search/routes.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, render_template, current_app, request, redirect
+import requests
+# from isodate import parse_duration
+
+main = Blueprint('main',__name__)
+
+
+
+@main.route('/', methods=['GET','POST'])
+def index():
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+    video_url = 'https://www.googleapis.com/youtube/v3/videos'
+    videos = []
+    if request.method == 'POST':
+
+        search_params = {
+            'key' : current_app.config['YOUTUBE_API_KEY'],
+            'q' : request.form.get('query'),
+            'part' : 'snippet',
+            'maxResults' : 9,
+            'type':'video'
+        }
+
+        r = requests.get(search_url, params=search_params)
+
+        results = r.json()['items']
+
+        video_ids = []
+        for result in results:
+            video_ids.append(result['id']['videoId'])
+
+        if request.form.get('submit') == 'lucky':
+            return redirect(f"https://www.youtube.com/watch?v={ video_ids[0] }")
+
+        video_params = {
+            'key': current_app.config['YOUTUBE_API_KEY'],
+            'id' : ','.join(video_ids),
+            'part' : 'snippet,contentDetails',
+            'maxResults':9,
+
+        }
+
+        r = requests.get(video_url,params=video_params)
+        results = r.json()['items']
+
+        for result in results:
+            video_data = {
+                'id': result['id'],
+                'url': f"https://youtube.com/watch?v={ result['id'] }",
+                'thumbnail': result['snippet']['thumbnails']['high']['url'],
+                'duration': (result['contentDetails']['duration']),
+                'title' : result['snippet']['title'],
+
+            }
+            videos.append(video_data)
+    
+    return render_template('index.html', videos=videos)
